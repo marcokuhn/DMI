40f32406bc2511c6a992352cd2dcae5f671fdc4b
weego <user@earweego.net>
2021-01-26 00:34:01 +0100

    code edits

--- code/ino/AnalogToMIDI_1/AnalogToMIDI_1.ino
+++ code/ino/AnalogToMIDI_1/AnalogToMIDI_1.ino
@@ -1,12 +1,10 @@
-/* DMI course, WS 2020-21 @ SRH
+/* DMI coursew, WS 2020-21 @ SRH
     hannes@earweego.net
     this is based on the "USB MIDI AnalogControlChange" Example frm teh Teensy library
-    written in Array synbtax for easier extension
 
-    Wiring: 4 times the wiring of the single channel example AnalogControlChange_0
-    Connect a wire each to A0 (14), A1, A2, A3 = 15, 16, 17,
-    GND (25) and 3,3V (24).
-    a high Ohm resistor (~1MOhm) between Ax and GND
+    Wiring:
+    Connect a wire each to A0 (14), GND (25) and 3,3V (24).
+    a high Ohm resistor (~1MOhm) between Ao and GND
     "measure resistance" by touching your hand between the hot pin and V+.
 
     send analog reading out as MIDI
@@ -19,49 +17,35 @@
 
 // the MIDI channel number to send messages
 const int channel = 1;
-const int framesPerSec = 50; // rate of measurements - 50 per sec is usually sufficient.
+const int framesPerSec = 50; // rate of measurements - 50 per sec is usually sufficient. 
 
-const byte numReads = 4;
-int analogPins[] = {A0, A1, A2, A3};
-const int ccs[] = {0, 1, 2, 3}; // send these CC nums
-int prevVals[] = {}; // cache previous readings
+// the MIDI continuous controller for each analog input
+const int controllerA0 = 0;  // CC 0
 
 void setup() {
-  Serial.begin(38400); // fast Serial port
-
-  for (byte i = 0; i <= numReads; i++) {
-    prevVals[i] = 0;
-  }
 }
 
+// store previously sent values, to detect changes
+int previousA0 = -1;
+int delayms = 1000 / framesPerSec;
 elapsedMillis msec = 0;
 
 void loop() {
-  // only check the analog inputs at 'framesPerSec' rate
+  // only check the analog inputs 50 times per second,
   // to prevent a flood of MIDI messages
-  if (msec >= 20) {
+  if (msec >= delayms) {
     msec = 0;
+    int n0 = analogRead(A0) / 8;
 
-    Serial.print("MIDI vals: ");  // print it on the Serial console
-
-    for (byte i = 0; i <= numReads; i++) {
-      int readVal = analogRead(analogPins[i]) >> 3; // reduce 10 bit to 7 bit
-      // only transmit MIDI messages if analog input changed
-      if (readVal != prevVals[i]) {
-        usbMIDI.sendControlChange(ccs[i], readVal, channel);
-        Serial.print("\tCC " );
-        Serial.print( ccs[i]);
-        Serial.print( ": " );
-        Serial.print( readVal );
-        prevVals[i] = readVal;
-      }
+    // only transmit MIDI messages if analog input changed
+    if (n0 != previousA0) {
+      usbMIDI.sendControlChange(controllerA0, n0, channel);
+      previousA0 = n0;
     }
-    Serial.println("");
   }
 
   // MIDI Controllers should discard incoming MIDI messages.
   // http://forum.pjrc.com/threads/24179-Teensy-3-Ableton-Analog-CC-causes-midi-crash
   while (usbMIDI.read()) {
-    // ignore incoming messages
   }
 }
